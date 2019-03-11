ccf1d0e5e5a2e7d704612ea0bf334554a0c1fc6f
Fioringo <shtuk.andrey@gmail.com>
2019-03-11 03:10:27 -0400

    Created methods

--- isPrime.cpp
+++ isPrime.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 #include <cmath>
 #include <chrono>
+#include <omp.h>
 
 using namespace std;
 using namespace std::chrono;
 
 string convertToString(bool a);
+// void makePrimeCache(long long a[1000]);
+
 bool isPrimeAS(long long a);
 bool isPrimeBS(long long a);
 bool isPrimeCS(long long a);
-bool isPrimeDS(long long a);
+// bool isPrimeDS(long long a, long long b[1000]);
 
 bool isPrimeAM(long long a);
 bool isPrimeBM(long long a);
 bool isPrimeCM(long long a);
-bool isPrimeDM(long long a);
+// bool isPrimeDM(long long a, long long b[1000]);
 
 int main(){
     bool as, am, bs, bm, cs, cm, ds, dm;
     duration<double> tas, tam, tbs, tbm, tcs, tcm, tds, tdm;
     long long a;
+
+    // long long primeCache[1000];
+
+    // makePrimeCache(primeCache);
 
     high_resolution_clock::time_point start;
     high_resolution_clock::time_point end;
@@ -63,12 +70,12 @@ int main(){
     cout << convertToString(cs) << endl;
     tcs = duration_cast<duration<double>>(end - start);
 
-    cout << "D: Single: " << endl;
-    start = high_resolution_clock::now();
-    ds = isPrimeDS(a);
-    end = high_resolution_clock::now();
-    cout << convertToString(ds) << endl;
-    tds = duration_cast<duration<double>>(end - start);
+    // cout << "D: Single: " << endl;
+    // start = high_resolution_clock::now();
+    // ds = isPrimeDS(a, primeCache);
+    // end = high_resolution_clock::now();
+    // cout << convertToString(ds) << endl;
+    // tds = duration_cast<duration<double>>(end - start);
 
 //=====================================================================
 // Multi-threaded calculations for a specific number to be prime
@@ -87,20 +94,164 @@ int main(){
     cout << convertToString(bm) << endl;
     tbm = duration_cast<duration<double>>(end - start);
 
-    cout << "A: Multi: " << endl;
+    cout << "C: Multi: " << endl;
     start = high_resolution_clock::now();
     cm = isPrimeCM(a);
     end = high_resolution_clock::now();
     cout << convertToString(cm) << endl;
     tcm = duration_cast<duration<double>>(end - start);
 
-    cout << "A: Multi: " << endl;
-    start = high_resolution_clock::now();
-    dm = isPrimeDM(a);
-    end = high_resolution_clock::now();
-    cout << convertToString(dm) << endl;
-    tdm = duration_cast<duration<double>>(end - start);
+    // cout << "A: Multi: " << endl;
+    // start = high_resolution_clock::now();
+    // dm = isPrimeDM(a, primeCache);
+    // end = high_resolution_clock::now();
+    // cout << convertToString(dm) << endl;
+    // tdm = duration_cast<duration<double>>(end - start);
+
+    cout << "-------------------------------" << endl;
+
+    cout << "AS: " << convertToString(as) << " " << tas.count() << endl;
+    cout << "BS: " << convertToString(bs) << " " << tbs.count() << endl;
+    cout << "CS: " << convertToString(cs) << " " << tcs.count() << endl;
+    // cout << "DS: " << convertToString(ds) << " " << tds.count() << endl;
+
+    cout << "-------------------------------" << endl;
+
+    cout << "AM: " << convertToString(am) << " " << tam.count() << endl;
+    cout << "BM: " << convertToString(dm) << " " << tdm.count() << endl;
+    cout << "CM: " << convertToString(cm) << " " << tcm.count() << endl;
+    // cout << "DM: " << convertToString(dm) << " " << tdm.count() << endl;
 
     return 0;
 }
 
+// 1000th prime number = 7919
+// void makePrimeCache(long long a[1000]){
+//     int counter = 0;
+//     for(int i = 2; i < 7920; ){
+//         if(isPrimeCS){
+//             a[counter] = i;
+//             ++counter;
+//         }
+//     }
+// }
+
+
+string convertToString(bool a){
+    if(a)
+        return "|   Is Prime!   |";
+    return "| Is not Prime! |";
+}
+
+//=====================================================================
+// Single threaded calculations for a specific number to be prime
+
+bool isPrimeAS(long long a){
+    for(int i = 2; i < a; ++i){
+        if(a % i == 0)
+            return false;
+    }
+    return true;
+}
+
+bool isPrimeBS(long long a){
+    if(a % 2 == 0)
+        return false;
+    for(int i = 3; i < a / 2 + 1; i = i + 2){
+        if(a % i == 0)
+            return false;
+    }
+    return true;
+}
+
+bool isPrimeCS(long long a){
+    long long b = sqrt(a);
+    if(a % 2 == 0 || a == b * b)
+        return false;
+    for(int i = 3; i < a / 2 + 1; i = i + 2){
+        if(a % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+// bool isPrimeDS(long long a, long long b[1000]){
+//     long long aa = sqrt(a);
+//     if(a % 2 == 0 || a == aa * aa)
+//         return false;
+//     if(a < 7920){
+//         for(int i = 0; i < 1000; ++i){
+//             if(b[i] == a)
+//                 return true;
+//             else
+//                 return false;
+//         }
+//     }
+//     for(int i = 7920; i < a / 2 + 1; i = i + 2){
+//         if(a % i == 0)
+//             return false;
+//     }
+//     return true;
+// }
+
+//=====================================================================
+// Multi-threaded calculations for a specific number to be prime
+
+bool isPrimeAM(long long a){
+    bool result = true;
+
+    #pragma omp parallel for
+    for(int i = 2; i < a; ++i){
+        if(a % i == 0)
+            result = false;
+    }
+    return result;
+}
+
+bool isPrimeBM(long long a){
+    bool result = true;
+    if(a % 2 == 0)
+        return false;
+    #pragma omp parallel for
+    for(int i = 3; i < a / 2 + 1; i = i + 2){
+        if(a % i == 0)
+            result = false;
+    }
+    return result;
+}
+
+bool isPrimeCM(long long a){
+    bool result = true;
+    long long b = sqrt(a);
+    if(a % 2 == 0 || a == b * b)
+        return false;
+    #pragma omp parallel for
+    for(int i = 3; i < a / 2 + 1; i = i + 2){
+        if(a % i == 0){
+            result = false;
+        }
+    }
+    return result;
+}
+
+// bool isPrimeDM(long long a, long long b[1000]){
+//     bool result = true;
+//     long long aa = sqrt(a);
+//     if(a % 2 == 0 || a == aa * aa)
+//         return false;
+//     if(a < 7920){
+//         #pragma omp parallel for
+//         for(int i = 0; i < 1000; ++i){
+//             if(b[i] == a)
+//                 result = true;
+//         }
+//     } else {
+//         #pragma omp parallel for
+//         for(int i = 7920; i < a / 2 + 1; i = i + 2){
+//             if(a % i == 0)
+//                 result = false;
+//         }
+//     }
+//     return result;
+// }
