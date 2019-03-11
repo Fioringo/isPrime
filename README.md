265a8976c336980a268e17e7e62851587a109b61
Fioringo <shtuk.andrey@gmail.com>
2019-03-11 02:15:16 -0400

    Initial commit 2

---
+++ README.md
@@ -0,0 +1,8 @@
+# isPrime
+<hr/>
+
+**Description**
+
+This console application displays performance difference of different approaches by running every approach on your computer.
+
+>Have fun!
--- isPrime.cpp
+++ isPrime.cpp
@@ -18,8 +18,11 @@ bool isPrimeDM(long long a);
 
 int main(){
     bool as, am, bs, bm, cs, cm, ds, dm;
-    duration<double> as, am, bs, bm, cs, cm, ds, dm;
+    duration<double> tas, tam, tbs, tbm, tcs, tcm, tds, tdm;
     long long a;
+
+    high_resolution_clock::time_point start;
+    high_resolution_clock::time_point end;
 
     cout << "===============================" << endl;
     cout << "=     Welcome to isPrime!     =" << endl;
@@ -27,7 +30,77 @@ int main(){
     cout << "Please enter a number: ";
     cin >> a;
 
-    
+    while(a < 3){
+        cout << "Please enter a number larger than 2: ";
+        cin >> a;
+    }
+
+    cout << "-------------------------------" << endl;
+    cout << "Number: " << a << endl;
+    cout << "-------------------------------" << endl;
+
+//=====================================================================
+// Single threaded calculations for a specific number to be prime
+
+    cout << "A: Single: " << endl;
+    start = high_resolution_clock::now();
+    as = isPrimeAS(a);
+    end = high_resolution_clock::now();
+    cout << convertToString(as) << endl;
+    tas = duration_cast<duration<double>>(end - start);
+
+    cout << "B: Single: " << endl;
+    start = high_resolution_clock::now();
+    bs = isPrimeBS(a);
+    end = high_resolution_clock::now();
+    cout << convertToString(bs) << endl;
+    tbs = duration_cast<duration<double>>(end - start);
+
+    cout << "C: Single: " << endl;
+    start = high_resolution_clock::now();
+    cs = isPrimeCS(a);
+    end = high_resolution_clock::now();
+    cout << convertToString(cs) << endl;
+    tcs = duration_cast<duration<double>>(end - start);
+
+    cout << "D: Single: " << endl;
+    start = high_resolution_clock::now();
+    ds = isPrimeDS(a);
+    end = high_resolution_clock::now();
+    cout << convertToString(ds) << endl;
+    tds = duration_cast<duration<double>>(end - start);
+
+//=====================================================================
+// Multi-threaded calculations for a specific number to be prime
+
+    cout << "A: Multi: " << endl;
+    start = high_resolution_clock::now();
+    am = isPrimeAM(a);
+    end = high_resolution_clock::now();
+    cout << convertToString(am) << endl;
+    tam = duration_cast<duration<double>>(end - start);
+
+    cout << "B: Multi: " << endl;
+    start = high_resolution_clock::now();
+    bm = isPrimeBM(a);
+    end = high_resolution_clock::now();
+    cout << convertToString(bm) << endl;
+    tbm = duration_cast<duration<double>>(end - start);
+
+    cout << "A: Multi: " << endl;
+    start = high_resolution_clock::now();
+    cm = isPrimeCM(a);
+    end = high_resolution_clock::now();
+    cout << convertToString(cm) << endl;
+    tcm = duration_cast<duration<double>>(end - start);
+
+    cout << "A: Multi: " << endl;
+    start = high_resolution_clock::now();
+    dm = isPrimeDM(a);
+    end = high_resolution_clock::now();
+    cout << convertToString(dm) << endl;
+    tdm = duration_cast<duration<double>>(end - start);
 
     return 0;
 }
+
